82fb4a119d768a63744da4c44b1490710b8d3dee
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-03-12 13:53:51 -0300

    3 tarjetas mas agregadas, y reduccion del texto por defecto

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types'
 import './card.css'
 
 
-function Card({title, imagen, text, url}) {
-  return (
-    <div className='card text-center bg-dark animate__animated animate__fadeInDown'>
-        <div className='overflow'>
-            <img src={imagen} alt='' className='card-img-top'/> 
+function Card({ title, imagen, text, url }) {
+    return (
+        <div className='card text-center bg-dark animate__animated animate__fadeInDown'>
+            <div className='overflow'>
+                <img src={imagen} alt='' className='card-img-top' />
+            </div>
+            <div className='card-body text-light'>
+                <h4 className='card-title'>{title}</h4>
+                <p className='card-text text-secondary'>
+                    {
+                        text ? text : `hsbdhbdscjsdbehbfsdbhbfdsjhhsbdhbdscjsdbeh`
+                    }
+                </p>
+                <a href={url} className='btn btn-outline-secondary rounded-0' target="_blank">
+                    Navegar
+                </a>
+            </div>
         </div>
-        <div className='card-body text-light'>
-            <h4 className='card-title'>{title}</h4>
-            <p className='card-text text-secondary'>
-                {
-                    text ? text : `hsbdhbdscjsdbehbfsdbhbfdsjhhsbdhbdscjsdbehbfsdbhbfds`
-                }
-            </p>
-            <a href={url} className='btn btn-outline-secondary rounded-0' target="_blank">
-                Navegar
-            </a>
-        </div>
-    </div>
-  )
+    )
 }
 
 Card.propTypes = {
